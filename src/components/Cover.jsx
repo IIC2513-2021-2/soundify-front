fd989aea6dd238e1571a7c9b659a71f62190b3c2
MFMG99 <maite.madalosso@uc.cl>
2021-11-15 16:33:32 -0300

    create react app

---
+++ src/components/Cover.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/index.scss';
+import Pathways from './Pathways.jsx';
+
+
+function Cover() {
+
+    return (
+        <section class="cover">
+            <h1>The most comprehensive artists catalogue</h1>
+            <Pathways />
+        </section>
+    );
+}
+
+export default Cover;
---
+++ src/components/Pathways.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/index.scss';
+
+
+function Cover() {
+
+    return (
+        <div class="pathways">
+            <a href="#Artists">Artists</a>
+            <a href="#Albums">Albums</a>
+        </div>
+    );
+}
+
+export default Cover;

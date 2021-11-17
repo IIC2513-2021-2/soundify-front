31dbe0c24e1b803c1f9406316843647f6738ea57
meretamal <meretamal@uc.cl>
2021-11-17 18:19:49 -0300

    feat(artists): add a request example to the artist detail view

--- src/views/ArtistDetail.jsx
+++ src/views/ArtistDetail.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from "react-router-dom";
 import Albums from "../components/Albums";
 
@@ -5,14 +6,46 @@ const ArtistDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
+  const [artist, setArtist] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then((response) => {
+      if (response.status !== 200) {
+        setError(true)
+      }
+      return response.json()
+    })
+    .then(setArtist)
+    .catch(() => setError(true))
+    .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <section className= 'container'>
+        <h2>Loading...</h2>
+      </section>
+    );
+  }
+
   return(
     <section className= 'container'>
       <Link to='/'>Home</Link>
-      <h2>{`Artist ${id}`}</h2>
-      <Albums />
-      <div>
-        <button onClick={() => navigate(-1)} type="button" className="button">Back</button>
-      </div>
+      {error ? (
+        <h2>Error</h2>
+      ) : (
+        <>
+          <h2>{artist?.name}</h2>
+          <Albums />
+          <div>
+            <button onClick={() => navigate(-1)} type="button" className="button">Back</button>
+          </div>
+        </>
+      )}
     </section>  
   )
 }
